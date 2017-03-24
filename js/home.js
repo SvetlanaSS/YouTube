52ab421eca248eff185a726abd3722eadeb2dacb
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-03-24 20:40:35 +0100

    minimized the function to get data

--- js/home.js
+++ js/home.js
@@ -1,84 +1,78 @@
 const youTubeKey = "AIzaSyCWBQyLAECpQ6IUQF_uVHLXXUWEjnypYWg";
+// Predifined data as array of objects
+const youTubeUsers = [
+  {
+    id: "UCdyTmTQTyGA2I7mMBODzqig",
+    userName: "#janni-name",
+    userDescription: "#janni-description",
+    userPhoto: "#janni-photo",
+    alt: "Jannis photo"
+  },
+  {
+    id: "UCZJr-W6Z40wfVnritqnp9kQ",
+    userName: "#ida-name",
+    userDescription: "#ida-description",
+    userPhoto: "#ida-photo",
+    alt: "Idas photo"
+  },
+  {
+    id: "UCPcx204312AHrBxgPSpFLmQ",
+    userName: "#kenza-name",
+    userDescription: "#kenza-description",
+    userPhoto: "#kenza-photo",
+    alt: "Kenzas photo"
+  },
+];
 
-// Code included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
+let ModuleGetUsersData = (function() {
+  let getYouTubeUsersData = () => {
+    youTubeUsers.map((user) => {
+      // Code included inside $(document).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
+      $(document).ready(function() {
+        /* getJSON - jQuery-function than load JSON-encoded data from the server using a GET HTTP request.
+        Link contains part, user id and key.*/
+        $.getJSON(`https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${user.id}&key=${youTubeKey}`, function(response) {
+          $.each(response.items, function(i, item) {
+            $(user.userPhoto).append(`<img src=${item.snippet.thumbnails.medium.url} alt=${user.alt}>`);
+            $(user.userName).append(item.snippet.title);
+            $(user.userDescription).append(item.snippet.description);
+          });
+        });
+      });
+    });
+  };
+
+  return {
+    getYouTubeUsersData: getYouTubeUsersData,
+  };
+})();
+
+ModuleGetUsersData.getYouTubeUsersData();
+
+
+
+
+
+/* If I have only one user, this is the function I would use. I wrote this function first. Then I realized that I would have three users.
+And I would repeat the same functions three times. I made an array of users and minimized lines of code.
+
 $(document).ready(function() {
-  /* jQuery - function than load JSON-encoded data from the server using a GET HTTP request.
-  Link contains part, user id and key.
-  JANNI*/
   $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=snippet&id=UCdyTmTQTyGA2I7mMBODzqig&key=AIzaSyCWBQyLAECpQ6IUQF_uVHLXXUWEjnypYWg",
-    // Function to handle json response data
     function(data) {
       $.each(data.items, function(i, item) {
-        // build the DOM-document, photo for Janni
         let userPhotoYouTubeJanni = item.snippet.thumbnails.medium.url;
         let userPhotoJanni = "<img src=" + userPhotoYouTubeJanni + "alt='Jannis photo'>";
         $("#janni-photo").append(userPhotoJanni);
 
-        // build the DOM-document, title for Janni
         let userNameYouTubeJanni = item.snippet.title;
         let userNameJanni = userNameYouTubeJanni;
         $("#janni-name").append(userNameJanni);
 
-        // build the DOM-document, description for Janni
         let userDescriptionYouTubeJanni = item.snippet.description;
         let userDescriptionJanni = userDescriptionYouTubeJanni;
         $("#janni-description").append(userDescriptionJanni);
       });
   });
-
-  // IDA, Link contains part, user name and key
-  $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=idasfitness&key=AIzaSyCWBQyLAECpQ6IUQF_uVHLXXUWEjnypYWg",
-    // Function to handle json response data
-    function(data) {
-      $.each(data.items, function(i, item) {
-        // build the DOM-document, photo for Ida
-        let userPhotoIda = "<article>";
-        let userPhotoYouTubeIda = item.snippet.thumbnails.medium.url;
-        userPhotoIda += "<div>"+userPhotoYouTubeIda+"</div>"+"<iframe src='https://yt3.ggpht.com/-Z2WsylQId20/AAAAAAAAAAI/AAAAAAAAAAA/f5BUij7kpKg/s240-c-k-no-mo-rj-c0xffffff/photo.jpg'></iframe>";
-        userPhotoIda += "</article>";
-        $("#ida-photo").append(userPhotoIda);
-
-        // build the DOM-document, title for Ida
-        let userNameIda = "<article>";
-        let userNameYouTubeIda = item.snippet.title;
-        userNameIda += "<h3>"+userNameYouTubeIda+"</h3>";
-        userNameIda += "</article>";
-        $("#ida-name").append(userNameIda);
-
-        // build the DOM-document, description for Ida
-        let userDescriptionIda = "<article>";
-        let userDescriptionYouTubeIda = item.snippet.description;
-        userDescriptionIda += "<p>"+userDescriptionYouTubeIda+"</p>";
-        userDescriptionIda += "</article>";
-        $("#ida-description").append(userDescriptionIda);
-      });
-  });
-
-  // KENZA, Link contains part, user name and key
-  $.getJSON("https://www.googleapis.com/youtube/v3/channels?part=snippet&forUsername=KenzaZouitenOfficial&key=AIzaSyCWBQyLAECpQ6IUQF_uVHLXXUWEjnypYWg",
-    // Function to handle json response data
-    function(data) {
-      $.each(data.items, function(i, item) {
-        // build the DOM-document, photo for Kenza
-        let userPhotoKenza = "<article>";
-        let userPhotoYouTubeKenza = item.snippet.thumbnails.medium.url;
-        userPhotoKenza += "<div>"+userPhotoYouTubeKenza+"</div>"+"<iframe src='https://yt3.ggpht.com/-QlPM7k67in8/AAAAAAAAAAI/AAAAAAAAAAA/85HIlh0Ousk/s240-c-k-no-mo-rj-c0xffffff/photo.jpg'></iframe>";
-        userPhotoKenza += "</article>";
-        $("#kenza-photo").append(userPhotoKenza);
-
-        // build the DOM-document, title for Kenza
-        let userNameKenza = "<article>";
-        let userNameYouTubeKenza = item.snippet.title;
-        userNameKenza += "<h3>"+userNameYouTubeKenza+"</h3>";
-        userNameKenza += "</article>";
-        $("#kenza-name").append(userNameKenza);
-
-        // build the DOM-document, description for Kenza
-        let userDescriptionKenza = "<article>";
-        let userDescriptionYouTubeKenza = item.snippet.description;
-        userDescriptionKenza += "<p>"+userDescriptionYouTubeKenza+"</p>";
-        userDescriptionKenza += "</article>";
-        $("#kenza-description").append(userDescriptionKenza);
-      });
-  });
 });
+
+*/
