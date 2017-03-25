0559bcfe75be362c26930158b17968c6de8ab275
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-03-25 16:46:30 +0100

    playlist's done

--- js/janni.js
+++ js/janni.js
@@ -9,8 +9,65 @@ const youTubeComments = [
   },
 ];
 
+
+
+// A Module for loading the latest videos
+let ModuleLatestVideos = (function() {
+  let getTheLatestVideos = () => {
+    // Code included inside $(document).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
+    $(document).ready(function() {
+      /* getJSON - jQuery-function than load JSON-encoded data from the server using a GET HTTP request.
+      Link contains part, channel id and key.*/
+      $.getJSON(``, function(response) {
+        $.each(response.items, function(i, item) {
+
+        });
+      });
+    });
+  };
+
+  return {
+    getTheLatestVideos:getTheLatestVideos,
+  }
+})();
+
+ModuleLatestVideos.getTheLatestVideos();
+
+
+
+// A Module for loading user's playlists
+let ModulePlaylists = (function() {
+  let getPlaylists = () => {
+    // Code included inside $(document).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
+    $(document).ready(function() {
+      /* getJSON - jQuery-function than load JSON-encoded data from the server using a GET HTTP request.
+      Link contains part, play list id (got from playlists) and key.*/
+      $.getJSON(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLS2NsUi0c62VfGbNe4OuqJy0Ms-W8N7A4&key=${youTubeKey}`, function(response) {
+        $.each(response.items, function(i, item) {
+          let html = `
+          <div class="center">
+            <p>${item.snippet.title}</p>
+            <iframe class="embed-responsive-item" src='http://www.youtube.com/embed/${item.snippet.resourceId.videoId}'></iframe>
+          </div>
+          `
+          $("#html-playlists").append(html);
+        });
+      });
+    });
+  };
+
+
+  return {
+    getPlaylists:getPlaylists,
+  }
+})();
+
+ModulePlaylists.getPlaylists();
+
+
+
 // A Module for loading descussions on user's channel
-let ModuleGetUserDescussion = (function() {
+let ModuleUserDescussion = (function() {
   let getYouTubeUserDescussion = () => {
     youTubeComments.map((author) => {
       // Code included inside $(document).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
@@ -30,7 +87,7 @@ let ModuleGetUserDescussion = (function() {
               </div>
             </div>
             `
-            $("#new-html").append(html);
+            $("#html-discussion").append(html);
           });
         });
       });
@@ -42,4 +99,4 @@ let ModuleGetUserDescussion = (function() {
   };
 })();
 
-ModuleGetUserDescussion.getYouTubeUserDescussion();
+ModuleUserDescussion.getYouTubeUserDescussion();
